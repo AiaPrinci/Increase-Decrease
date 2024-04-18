cb6745be622f336f938be5e5ec2d46db4bcba590
AiaPrinci <davidjimsheleishvili17@gmail.com>
2024-04-18 18:07:01 +0400

    first commit

--- Script/script.js
+++ Script/script.js
@@ -1,17 +1,19 @@
 let number = document.querySelector('.number');
+let hud = ['gora', 'woma', 'balaxvani', 'zastava', 'saficxia'];
+let hudLength = hud.length;
+number.innerHTML = hudLength - 5;
 
-let button1 = document.querySelector('#button1');
-
-let button2 = document.querySelector('#button2');
-
-let button3 = document.querySelector('#button3');
 
 let i = 0;
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
+let button3 = document.querySelector('#button3');
+
 
 button1.addEventListener('click', ()=>{
     if(i >= 0 && i <= 9){
         i++
-        document.querySelector('.number').innerHTML = i;
+        number.innerHTML = i;
     }
 })
 
